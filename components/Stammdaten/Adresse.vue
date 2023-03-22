df0e8e95ef650398b58a8bfe939b4f1fd237cddc
Alex Staenke <alex@elementarteilchen.at>
2023-03-22 08:22:53 +0100

    Neues Sidebar Layout statt der 2. Tab Reihe

--- components/Stammdaten/Adresse.vue
+++ components/Stammdaten/Adresse.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-  <div>
-    <h3 class="text-lg font-bold text-gray-700 mt-8 mb-2">Meldeadresse</h3>
+  <h3 class="text-lg font-bold text-gray-900 ">Adressen</h3>
+  <div class="bg-gray-100 p-4 rounded">
+    <h3 class="text-lg_ font-bold text-gray-700 mb-2">Meldeadresse</h3>
       <div class="grid lg:grid-cols-2 gap-4">
         <div class=" grid grid-cols-[minmax(6rem,auto)_1fr] gap-2">
             <InputText label="Straße" value="Hauptstraße 23/5/4" />
--- pages/hra-formular-sidebar.vue
+++ pages/hra-formular-sidebar.vue
@@ -17,11 +17,13 @@
 
 <template>
   
-  <Suche />
-  <div class="mt-4 w-100">
+  <!-- <Suche /> -->
+  <div class="mt-2 w-100">
     <div class="bg-white p-4 border rounded flex items-center justify-between">
       <div class="font-bold">
-        Eintrittsdaten für Fr. Babunek Stephanie bearbeiten
+        Eintrittsdaten für Fr. 
+        <a href="" class="hover:underline text-blue-600">
+          Babunek Stephanie</a> bearbeiten
       </div>
       <div>
         <a class="p-2 text-blue-600 hover:underline inline-flex" href=""> 
@@ -31,23 +33,23 @@
     </div>
     <div>
 
-    <div class="px-4_ py-6_ mt-4 bg-white border rounded overflow-hidden">      
-      <nav class="bg-white border-b ">
+    <div class="mt-6 bg-white border rounded overflow-hidden">      
+      <nav class="bg-white border-b_ ">
         <ul class="flex space-x-4">
           <li class="px-4 py-2 bg-blue-700 text-white cursor-pointer">Stammdaten</li>
           <li class="px-4 py-2 hover:bg-blue-700 hover:text-white cursor-pointer">Schulbildung</li>
           <li class="px-4 py-2 hover:border-blue-600 cursor-pointer">Berufsweg</li>
         </ul>
       </nav>
+    </div>
 
-      <main class="mt-8 px-4">
-        <h3 class="font-bold text-lg px-2 py-2">Persönlich</h3>
-        <div class="px-2 py-4">
-          
-          <nav class="text-sm bg-white  border-gray-400">
-            <ul class="flex items-center space-x-4 text-sm">
+    <div class="mt-4 bg-white border border-t-4 rounded overflow-hidden">      
+      <main class="grid grid-cols-[10rem_1fr]" >
+        <div class="mt-4">
+          <nav class="text-sm ">
+            <ul class="text-sm space-y-1_">
               <li v-for="tab, index in tabs" 
-                  class="px-2 py-2 flex"
+                  class="pl-4 pr-8 py-3 flex"
                   :class="[{'bg-gray-400 text-white font-bold': tab == activeTab.tab}]">
                   <a href="#" @click="activeTab.tab = tab">
                       {{tab}}</a>
@@ -57,11 +59,12 @@
         
         </div>
 
-          <div v-show="tabs[0] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-16">
-            <StammdatenPersoenlich />
+        <div class="px-4 py-4 border-l">
+          <div v-show="tabs[0] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-4">
+            <StammdatenPersoenlich02 />
             <StammdatenLand />
           </div>
-          <div v-show="tabs[1] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-8">
+          <div v-show="tabs[1] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-4">
             <StammdatenAdresse />
           </div>
           <div v-show="tabs[2] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-8">
@@ -78,9 +81,10 @@
           </div>
         
 
-        <div class="p-2 mt-6 flex justify-start space-x-4 items-center text-sm">
-          <a href="" class="text-blue-600 hover:underline">Abbrechen</a>
-          <button class="px-2 py-2 bg-blue-600 text-white hover:bg-blue-700">Speichern</button>
+          <div class="p-2 mt-6 flex justify-start space-x-4 items-center text-sm">
+            <a href="" class="text-blue-600 hover:underline">Abbrechen</a>
+            <button class="px-2 py-2 bg-blue-600 text-white hover:bg-blue-700">Speichern</button>
+          </div>
         </div>
 
       </main>
