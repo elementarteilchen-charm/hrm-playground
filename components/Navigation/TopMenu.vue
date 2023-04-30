85a7dc69043e380d4e5d3ba693ae45b58c17647e
Alex <alex@elementarteilchen.at>
2023-04-30 06:57:12 +0200

    WIP

--- components/Navigation/TopMenu.vue
+++ components/Navigation/TopMenu.vue
@@ -1,25 +1,28 @@
 <script setup>
-    import { reactive } from 'vue'
-    const props = defineProps({
-            topMenuItems: {type: Array}, 
-            activeMenuItem: Object
-          });
- 
-   let activeMenuItem = props.activeMenuItem;
-   
+
+const props = defineProps({
+        topMenuItems: {type: Array}, 
+        active: {type: Number, default: 0}
+      });
+    
 </script>
 
 <template>
-    <div class="bg-white border rounded overflow-hidden">
-      <nav class="">
-        <ul class="flex gap-4">
-            <li v-for="item,index in topMenuItems" 
-                    class="px-4 py-2 hover:bg-blue-700 hover:text-white cursor-pointer" 
-                    :class="[{'bg-blue-700 text-white': activeMenuItem.active == item.active}]">
-                <a :href="item.link" >
-                     {{item.linkText}}</a>
-            </li>
-        </ul>
-      </nav>
+    <div class="flex justify-between">
+        
+        <div class="bg-white border rounded overflow-hidden">
+          <nav class="">
+            <ul class="flex gap-4">
+                <li v-for="item,index in topMenuItems" 
+                        class="px-4 py-2 hover:bg-blue-700 hover:text-white cursor-pointer"                    
+                        :class="[{'bg-blue-700 text-white': active == index}]">
+                    <a :href="item.link">
+                         {{item.linkText}}</a>
+                </li>
+            </ul>
+          </nav>
+        </div>
+
+        <button class="bg-blue-700 text-white px-4 py-2 border border-blue-700">Eintritt abschließen</button>
     </div>
 </template>
---
+++ pages/hra-vertragsdaten.vue
@@ -0,0 +1,64 @@
+<script setup>
+  import { ArrowRightCircleIcon, ArrowDownCircleIcon, PlusCircleIcon, ChevronRightIcon } from '@heroicons/vue/24/outline' 
+
+  const tabs = [
+        'Vertrag', 
+        'Zusatzvereinbarungen',
+    ];
+  const activeTab = reactive({ tab: tabs[0] });
+
+</script>
+
+<template>
+  
+  <main class="grid grid-rows-[auto_auto_auto] gap-8">
+    
+    <EintretendePersonHeader vorname="Stephanie" anrede="Fr." nachname="Babunek"/>
+    <NavigationTopMenu :topMenuItems="topMenuItems" :active="3" />
+
+    <div class="bg-white border border-t rounded overflow-hidden">
+
+      <main class="grid lg:grid-cols-[minmax(12rem,auto)_1fr]">
+          
+        <nav class="mt-4">
+          <ul class="text-sm">
+            <li v-for="tab, index in tabs" 
+                class="border-l-4 border-transparent pl-4 pr-8 py-3 flex hover:bg-gray-200"
+                :class="[{'border-l-4 border-blue-500 text-blue-700 font-bold': tab == activeTab.tab}]">
+                <a href="#" @click="activeTab.tab = tab">
+                  {{tab}}</a>
+            </li>
+          </ul>
+
+          <div class="mt-4 border-t border-b px-2 py-6 flex flex-col space-y-3">
+            <a href="" class="px-3 py-2 bg-blue-700 text-white text-center">
+              Speichern
+            </a>
+            <a href="" class="px-3 py-2 text-blue-700 hover:underline text-center">
+              Abbrechen
+            </a>
+          </div>
+        </nav>
+        
+        <div class="px-4 py-4 border-l pb-12">
+
+          <div v-show="tabs[0] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-4">
+            <StammdatenPersoenlich />
+            <StammdatenLand />
+            <StammdatenFamilienstand />
+          </div>
+          <div v-show="tabs[1] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-4">
+            <StammdatenAdresse />
+          </div>
+          <div v-show="tabs[3] == activeTab.tab" class="px-2 pt-2 grid grid-rows-1 gap-4">
+            <StammdatenFamilienangehoerige />
+          </div>
+
+        </div>
+
+      </main>
+    </div>
+  
+  </main>
+
+</template>
