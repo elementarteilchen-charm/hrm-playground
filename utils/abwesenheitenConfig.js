1e5f6632597c31d3ca2e0d8691c1a60ac9cbd14f
Alex <alex@elementarteilchen.at>
2024-06-09 10:57:11 +0200

    Abwesenheiten in config ausgelagert

---
+++ utils/abwesenheitenConfig.js
@@ -0,0 +1,50 @@
+export const abwesenheitenListe = [
+        'Elternkarenz',
+        'Familienzeit',
+        'Bildungskarenz',
+        'Langzeitkrankenstand',
+        'Unbezahlter Urlaub',
+        'Wehrdienst',
+        'Milizübung'
+      ]
+export  const abwesenheitenLinks = {
+        'Elternkarenz': '/hra/abwesenheiten/elternkarenz',
+        'Familienzeit': '/hra/abwesenheiten/familienzeit',
+        'Bildungskarenz': '/hra/abwesenheiten/bildungskarenz',
+        'Langzeitkrankenstand': '/hra/abwesenheiten/langzeitkrankenstand',
+        'Unbezahlter Urlaub': '/hra/abwesenheiten/unbezahlterurlaub',
+        'Wehrdienst': '/hra/abwesenheiten/wehrdienst',
+        'Milizübung': '/hra/abwesenheiten/milizuebung' 
+    }
+
+export const abwesenheitenMenuItems = [
+      {
+        linkText: "Elternkarenz",
+        link: "/hra/abwesenheiten/elternkarenz",
+      },
+      {
+        linkText: "Familienzeit",
+        link: "/hra/abwesenheiten/familienzeit",
+      },
+      {
+        linkText: "Bildungskarenz",
+        link: "/hra/abwesenheiten/bildungskarenz",
+      },
+      {
+        linkText: "Milizübung",
+        link: "/hra/abwesenheiten/milizuebung",
+      },
+      {
+        linkText: "Wehr- & Zivildienst  ",
+        link: "/hra/abwesenheiten/wehrdienst",
+      },
+      {
+        linkText: "Unbezahlter Urlaub",
+        link: "/hra/abwesenheiten/unbezahlterurlaub",
+      },
+      {
+        linkText: "Langzeit Krankenstand ",
+        link: "/hra/abwesenheiten/langzeitkrankenstand",
+      },
+
+    ];
